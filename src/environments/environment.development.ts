4bdcb0d44628b2bec151f37cbad2584f87d062e9
PatrickAsiedu <pattribette@gmail.com>
2025-11-19 18:10:29 +0000

    changed prod url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseURL: 'http://localhost:3003',
-  webSocketUrl:'ws://1ocalhost:3003'
+  baseURL: 'http://localhost:8081',
+  webSocketUrl:'ws://1ocalhost:8081'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseURL: 'http://13.51.13.29:8081',
-  webSocketUrl: 'ws://13.51.13.29:8081',
+  baseURL: 'https://tradeflowapp.xyz',
+  webSocketUrl: 'wss://tradeflowapp.xyz',
 };
